a446b11b38e5a7b5e4c0e77f7d05e2257c8e0b06
Ubuntu <ubuntu@ip-172-31-37-4.ec2.internal>
2019-07-12 09:59:43 +0000

    Skeleton Update

---
+++ Mirko_H-L.cpp
@@ -0,0 +1,28 @@
+#property copyright "Copyright © 2019, Mirko Primatici."
+
+#property indicator_chart_window
+#property indicator_buffers 2
+#property indicator_color1 DarkBlue
+#property indicator_color2 DarkBlue
+
+extern int       Range_n=20; //Customizable 
+
+
+double HiBuffer[],LoBuffer[];
+
+int init()
+  {
+   
+  }
+
+int deinit()
+  {
+
+  
+  }
+
+int start()
+  {
+   
+   return(0);
+  }
